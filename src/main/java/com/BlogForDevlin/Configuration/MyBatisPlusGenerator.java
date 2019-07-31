ea1fdf77f4e6a0efa32248bc92b6cb3b51d85ede
Devlin-Kim <JustForCaprice@outlook.com>
2019-07-31 17:11:59 +0800

    PersonalDB v1.1     edit mybatisPlus configure.

---
+++ src/main/java/com/BlogForDevlin/Configuration/MyBatisPlusGenerator.java
@@ -0,0 +1,146 @@
+package com.BlogForDevlin.Configuration;
+
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.converts.MySqlTypeConvert;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.DbColumnType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @author Jinyl
+ * @date 2019/7/31
+ * @Mail Jinyl@mail.taiji.com.cn
+ * @Describe:
+ */
+public class MyBatisPlusGenerator {
+
+    /*@Value("${DBurl}")
+    static private String DBurl;*/
+    /**
+     * <p>
+     * 读取控制台内容
+     * </p>
+     */
+    public static String scanner(String tip) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder help = new StringBuilder();
+        help.append("请输入" + tip + "：");
+        System.out.println(help.toString());
+        if (scanner.hasNext()) {
+            String ipt = scanner.next();
+            if (StringUtils.isNotEmpty(ipt)) {
+                return ipt;
+            }
+        }
+        throw new MybatisPlusException("请输入正确的" + tip + "！");
+    }
+
+    public static void main(String[] args) {
+        // 代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setDbType(DbType.MYSQL);
+        dsc.setUrl("jdbc:mysql://localhost:7676/item?nullCatalogMeansCurrent=true&useUnicode=true&characterEncoding=utf-8&useSSL=false&serverTimezone=GMT%2B8");
+        dsc.setDriverName("com.mysql.cj.jdbc.Driver");
+        //dsc.setSchemaName("item");
+        dsc.setUsername("Devlin");
+        dsc.setPassword("Alpha.1111");
+
+        dsc.setTypeConvert(new MySqlTypeConvert() {
+            @Override
+            public DbColumnType processTypeConvert(GlobalConfig globalConfig, String fieldType) {
+                System.out.println("转换类型：" + fieldType);
+                //tinyint转换成Boolean
+                if (fieldType.toLowerCase().contains("date")) {
+                    return DbColumnType.DATE;
+                }
+                //将数据库中datetime转换成date
+                if (fieldType.toLowerCase().contains("datetime")) {
+                    return DbColumnType.DATE;
+                }
+                return (DbColumnType) super.processTypeConvert(globalConfig, fieldType);
+            }
+        });
+
+        mpg.setDataSource(dsc);
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        String projectPath = System.getProperty("user.dir");
+        gc.setOutputDir(projectPath + "/src/main/java");
+        gc.setAuthor("Jin");
+        gc.setOpen(false);
+        gc.setSwagger2(true);
+        //gc.setActiveRecord(true);
+        gc.setFileOverride(true);
+//        gc.setServiceName("%sExt");
+//        gc.setServiceImplName("%sExtImpl");
+        mpg.setGlobalConfig(gc);
+
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setParent("taiji.item");
+        pc.setEntity("model.auto");
+        pc.setMapper("mapper.auto");
+
+        mpg.setPackageInfo(pc);
+
+        // 自定义配置
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
+
+        // 配置模板
+        TemplateConfig templateConfig = new TemplateConfig();
+
+        templateConfig.setEntity("itemplates/entity.java");
+        templateConfig.setMapper("itemplates/mapper.java");
+        templateConfig.setController(null);
+        templateConfig.setService(null);
+        templateConfig.setServiceImpl(null);
+        templateConfig.setXml(null);
+        mpg.setTemplate(templateConfig);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        //strategy.setInclude(scanner("表名"));
+        strategy.setInclude("s_zhuxiang");
+
+        strategy.setSuperEntityClass("com.baomidou.mybatisplus.extension.activerecord.Model");
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        //strategy.setSuperEntityColumns("gmt_create", "gmt_modified");
+
+        strategy.setEntityLombokModel(true);
+        strategy.setEntityTableFieldAnnotationEnable(true);
+
+        strategy.setRestControllerStyle(true);
+        strategy.setControllerMappingHyphenStyle(true);
+
+        //Boolean类型字段是否移除is前缀（默认 false）
+        strategy.setEntityBooleanColumnRemoveIsPrefix(true);
+
+        mpg.setStrategy(strategy);
+        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+        mpg.execute();
+    }
+}
--- src/main/java/com/BlogForDevlin/personalblog/PersonalblogApplication.java
+++ src/main/java/com/BlogForDevlin/personalblog/PersonalblogApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.BlogForDevlin.mapper")
+@MapperScan("com.BlogForDevlin.MapperXml")
 public class PersonalblogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PersonalblogApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalblogApplication.class, args);
+    }
 
 }
